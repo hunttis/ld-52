d49989c0a298671ab8e8c33cbe3fae9e67fdcbe4
Ville Nuutinen <hunttis@me.com>
2022-09-28 17:11:02 +0300

    Make hot reload optional. Define target build directory

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { GameScene } from "./scenes/gameScene";
+import "vite/types/importMeta.d"; // Not needed when not using TypeScript
+
+const hotReload = true;
 
 export function startGame() {
   const config: Phaser.Types.Core.GameConfig = {
@@ -7,11 +10,18 @@ export function startGame() {
       mode: Phaser.Scale.FIT,
       autoCenter: Phaser.Scale.CENTER_BOTH,
       width: 800,
-      height: 800,
+      height: 600,
     },
     parent: "game",
     scene: [GameScene],
   };
 
   return new Phaser.Game(config);
+}
+
+// This prevents hot reload, which is useful when you're developing in live share
+if (!hotReload && import.meta.hot) {
+  import.meta.hot.on("vite:beforeFullReload", () => {
+    throw "(skipping full reload)";
+  });
 }
